4c0ec5730f49e8e2f54bc5c29129dcbae6bde89f
AlexAGit <alex.achim@execview.co.uk>
2016-11-16 22:20:33 +0000

    first commit

---
+++ ImageGallery.DataAccess/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ImageGallery.ApiDataAccess.Models
+{
+    [Serializable]
+    public class Response<T>
+    {
+        public T Data
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ImageGallery/Controllers/GalleryController.cs
@@ -0,0 +1,32 @@
+﻿using ImageGallery.ApiDataAccess.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            var repository = new GalleryRepository();
+             
+            return View(repository.GetCategories());
+        }
+
+        public ActionResult CategoryImages(string categoryName, int itemsPerPage = 5)
+        {
+            var repository = new GalleryRepository();
+
+            return View(repository.GetImagesForCategory(categoryName, itemsPerPage));
+        }
+    }
+}
